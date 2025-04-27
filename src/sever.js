d508ca55512f1e8082338d4e4ac7226c0de3fde6
alexhuynh <121414952+alexhuynh01@users.noreply.github.com>
2025-04-27 13:56:12 +0700

    Init code

---
+++ src/sever.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+import express from 'express'
+
+const app = express()
+const hostname = 'localhost'
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
